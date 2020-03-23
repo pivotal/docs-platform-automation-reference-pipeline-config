680f6a53d1fdbeeb256eca5fb6bbf49c140c12b2
Jeremy Alvis <jalvis@pivotal.io>
2020-03-23 09:33:16 -0600

    update script to only set the pks pipeline
    Signed-off-by: Kira Boyle <kboyle@pivotal.io>

--- scripts/update-reference-pipeline.sh
+++ scripts/update-reference-pipeline.sh
@@ -12,20 +12,20 @@ echo "Setting sandbox reference pipeline..."
 
 # Adds a tag to every job to tell the pipeline what remote worker to run on
 # for this pipeline, the remote worker is named 'vsphere'
-fly -t ci sp -p reference-pipeline-pks \
+fly -t ci sp -p reference-pipeline \
   -c <(cat ~/workspace/docs-platform-automation-reference-pipeline-config/pipelines/pipeline.yml |
 	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
 	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere"]} else . end)') \
   --var foundation=sandbox \
   --check-creds
 
-echo "Setting development reference pipeline..."
+#echo "Setting development reference pipeline..."
 
 # Adds a tag to every job to tell the pipeline what remote worker to run on
 # for this pipeline, the remote worker is named 'vsphere'
-fly -t ci sp -p reference-pipeline-development \
-  -c <(cat ~/workspace/docs-platform-automation-reference-pipeline-config/pipelines/pipeline.yml |
-	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
-	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere"]} else . end)') \
-  --var foundation=development \
-  --check-creds
+#fly -t ci sp -p reference-pipeline-development \
+#  -c <(cat ~/workspace/docs-platform-automation-reference-pipeline-config/pipelines/pipeline.yml |
+#	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
+#	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere"]} else . end)') \
+#  --var foundation=development \
+#  --check-creds
