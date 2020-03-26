a67b9f2b26179b55b8eaa23b693d1aeeb01f4f03
Kira Boyle <kboyle@pivotal.io>
2020-03-26 11:10:33 -0600

    Change opsman name [#171704450]
    Signed-off-by: Jeremy Alvis <jalvis@pivotal.io>

--- scripts/update-reference-pipeline.sh
+++ scripts/update-reference-pipeline.sh
@@ -11,21 +11,21 @@ fly -t ci sp -p reference-resources \
 echo "Setting sandbox reference pipeline..."
 
 # Adds a tag to every job to tell the pipeline what remote worker to run on
-# for this pipeline, the remote worker is named 'vsphere'
+# for this pipeline, the remote worker is named 'openstack' because it is in the same environment as our vSphere
 fly -t ci sp -p reference-pipeline \
   -c <(cat ~/workspace/docs-platform-automation-reference-pipeline-config/pipelines/pipeline.yml |
 	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
-	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere"]} else . end)') \
+	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["openstack"]} else . end)') \
   --var foundation=sandbox \
   --check-creds
 
 #echo "Setting development reference pipeline..."
 
 # Adds a tag to every job to tell the pipeline what remote worker to run on
-# for this pipeline, the remote worker is named 'vsphere'
+# for this pipeline, the remote worker is named 'openstack' because it is in the same environment as our vSphere
 #fly -t ci sp -p reference-pipeline-development \
 #  -c <(cat ~/workspace/docs-platform-automation-reference-pipeline-config/pipelines/pipeline.yml |
 #	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
-#	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere"]} else . end)') \
+#	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["openstack"]} else . end)') \
 #  --var foundation=development \
 #  --check-creds
