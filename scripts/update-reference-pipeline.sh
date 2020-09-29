199c39425465c5091e4f41140233dc412b5f77df
JT Archie <jtarchie@gmail.com>
2020-09-29 17:01:04 -0400

    WIP

--- scripts/update-reference-pipeline.sh
+++ scripts/update-reference-pipeline.sh
@@ -13,8 +13,6 @@ echo "Setting sandbox reference pipeline..."
 # Adds a tag to every job to tell the pipeline what remote worker to run on
 # for this pipeline, the remote worker is named 'vsphere-pez' because it is in the same environment as our vSphere
 fly -t ci sp -p reference-pipeline \
-  -c <(cat ./pipelines/pipeline.yml |
-	ruby -ryaml -rjson -e "puts YAML.load(STDIN.read).to_json" |
-	jq 'walk(if type == "object" and (has("source") or has("get") or has("task") or has("put")) then . + {tags: ["vsphere-pez"]} else . end)') \
+  -c  ./pipelines/pipeline.yml \
   --var foundation=sandbox \
   --check-creds
