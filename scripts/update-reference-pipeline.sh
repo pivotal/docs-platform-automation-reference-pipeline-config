8b96b73c9f9fbb54ee40ad8c763fc03ab0410337
Mark Stokan <stokanm@vmware.com>
2021-10-27 15:35:32 -0700

    replace "ci" target with "platform-automation"
    * "ci" is too generic of a target name and our ppe-workstation already
      has a "platform-automation" target set up for us.
    
    Authored-by: Mark Stokan <stokanm@vmware.com>
    
    [#180029246] bump versions of OpsMgr + TAS in PA reference pipeline

--- scripts/update-reference-pipeline.sh
+++ scripts/update-reference-pipeline.sh
@@ -4,7 +4,7 @@ set -eu
 
 echo "Setting download products pipeline..."
 
-fly -t ci sp -p reference-resources \
+fly -t platform-automation sp -p reference-resources \
   -c ./pipelines/download-products.yml \
   --check-creds
 
@@ -12,7 +12,7 @@ echo "Setting sandbox reference pipeline..."
 
 # Adds a tag to every job to tell the pipeline what remote worker to run on
 # for this pipeline, the remote worker is named 'vsphere-pez' because it is in the same environment as our vSphere
-fly -t ci sp -p reference-pipeline \
+fly -t platform-automation sp -p reference-pipeline \
   -c  ./pipelines/pipeline.yml \
   --var foundation=sandbox \
   --check-creds
